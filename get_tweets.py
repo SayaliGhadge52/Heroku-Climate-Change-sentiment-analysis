5607f1bb862e80db5a5253bc3bac547ea719f637
SayaliGhadage <68659608+SayaliGhadge52@users.noreply.github.com>
2021-04-22 21:20:35 +0530

    Update get_tweets.py

--- get_tweets.py
+++ get_tweets.py
@@ -4,13 +4,13 @@ import pandas as pd
 pd.set_option('display.max_colwidth', 1000)
 
 # api key
-api_key = ""
+api_key = "EmSGfrdY28btB3kEFlkayJGI5"
 # api secret key
-api_secret_key = ""
+api_secret_key = "OmFNtqo8NyQBuQfDa9mL0L4TUiTkWeACGGpD3aUlvPRSICiFXM"
 # access token
-access_token = ""
+access_token = "931297071847448576-5w5I5uylTSvOF4t9DII78Yy3FL0cmyD"
 # access token secret
-access_token_secret = ""
+access_token_secret = "hCihGWmkFEHo3jUV5ksLpi8PHIeeZxshXt84d95uEuXIg"
 
 authentication = tweepy.OAuthHandler(api_key, api_secret_key)
 authentication.set_access_token(access_token, access_token_secret)
